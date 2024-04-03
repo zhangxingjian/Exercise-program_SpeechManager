ea8acbdf3e03e1810879ff9c6fcb65780da6fcc1
zhangxingjian <zhangxj2418@outlook.com>
2024-04-03 19:10:21 +0800

    创建选手，并测试

--- 演讲比赛/speechManager.h
+++ 演讲比赛/speechManager.h
@@ -1,5 +1,8 @@
 #pragma once
 #include<iostream>
+#include<vector>
+#include<map>
+#include"speaker.h"
 using namespace std;
 
 class SpeechManager {
@@ -8,9 +11,25 @@ public:
 	SpeechManager();
 
 	void show_Menu();
-
-
-
 	void exitSystem();
+
+	void initSpeech();
+
+	void createSpeaker();
+
+
+
 	~SpeechManager();
+
+
+	vector<int>v1;
+
+	vector<int>v2;
+
+	vector<int>vVictory;
+
+	map<int, Speaker>m_Speaker;
+
+	int m_Index;
+
 };
