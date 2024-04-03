08fe8c4176c104a52efb94cdeb19aae72edd53f8
zhangxingjian <zhangxj2418@outlook.com>
2024-04-03 18:10:16 +0800

    管理类创建

---
+++ 演讲比赛/main.cpp
@@ -0,0 +1,33 @@
+#include"speechManager.h"
+
+int main() {
+	SpeechManager sm;
+	int choice = 0;
+
+	while (true)
+	{
+		sm.show_Menu();
+		cout << "请输入您的选择：" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:  //开始比赛
+			break;
+		case 2:  //查看记录
+			break;
+		case 3:  //清空记录
+			break;
+		case 0:  //退出系统
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
+
+
+	system("pause");
+	return 0;
+
+}
